1a894dc53c528cdf2ec5e761b77adf66f5ae70e8
DH_xlx <v847588615@hotmail.com>
2025-03-08 10:26:25 +0800

    fi frame

--- backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
@@ -3,12 +3,18 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotinfoController {
 
     @RequestMapping("getbotinfo/")
-    public String getbotinfor(){
-        return "hhhh";
+    public Map<String,String> getbotinfor(){
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("rating","123");
+        return bot1;
     }
 }
